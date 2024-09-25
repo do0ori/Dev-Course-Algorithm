7e47716c9585b49ef3d5eb25593aa63dd11d68a6
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2024-09-25 23:36:42 +0900

    [level 2] Title: 프로세스, Time: 0.68 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,18 +1,20 @@
 from collections import deque
 
-def solution(priorities, location):
-    
-    queue = deque([(priority, idx) for idx, priority in enumerate(priorities)])
-    process_cnt = 0
-    
-    while queue:
-        current = queue.popleft()
-        
-        if any(current[0] < item[0] for item in queue):
-            queue.append(current)
-        else :
-            process_cnt += 1
-            if current[1] == location:
-                return process_cnt
 
-    return process_cnt
+def solution(priorities, location):
+    count = 0
+    queue = deque([[p, i] for i, p in enumerate(priorities)])
+
+    while queue:
+        process = queue.popleft()
+        
+        # any를 사용하면 True일 경우 즉시 반환하지만 다음과 같이 max를 사용하면 항상 O(n)
+        # if process[0] < max(queue, key=lambda x: x[0])[0]:
+        if any(process[0] < p[0] for p in queue):
+            queue.append(process)
+        else:
+            count += 1
+            if process[1] == location:
+                return count
+    
+    return count
