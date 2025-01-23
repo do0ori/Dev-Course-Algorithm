01fea2d75f431c21477bbbf5812db6906b98ab3d
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2025-01-23 23:56:40 +0900

    [level 2] Title: 큰 수 만들기, Time: 134.22 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,6 +1,7 @@
 def solution(number, k):
     stack = []
     
+    # 현재 수보다 앞에 있는 작은 수 모두 제거
     for n in number:
         while stack and stack[-1] < n and k != 0:
             stack.pop()
@@ -8,6 +9,7 @@ def solution(number, k):
 
         stack.append(n)
     
+    # k개 만큼 제거되지 않은 경우 뒤에서 제거
     return ''.join(stack if not k else stack[:-k])
     
     
