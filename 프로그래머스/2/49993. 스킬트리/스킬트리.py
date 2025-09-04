91c7190472be1f79d15b2f144ef5db046d27e1e9
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2025-09-04 13:58:34 +0900

    [level 2] Title: 스킬트리, Time: 0.02 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,7 +1,24 @@
+# 문자열로 처리
+"""
 def solution(skill, skill_trees):
     answer = 0
     for skill_tree in skill_trees:
         filtered_skill_tree = ''.join(filter(lambda x: x in skill, skill_tree))
         if skill.find(filtered_skill_tree) == 0:
             answer += 1
+    return answer
+"""
+# 순서체크
+def solution(skill, skill_trees):
+    answer = 0
+    
+    for skill_tree in skill_trees:
+        skill_list = list(skill)
+        for s in skill_tree:
+            if s in skill_list:
+                if skill_list.pop(0) != s:
+                    break
+        else:   # break 없이 모두 순회 완료하면
+            answer += 1
+    
     return answer
