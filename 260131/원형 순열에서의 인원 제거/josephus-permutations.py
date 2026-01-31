a8a158cff9e70b4e1c68674d4852bfaff017ca0f
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2026-01-31 22:46:20 +0900

    Title: 원형 순열에서의 인원 제거, Time: 74ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 260131/원형 순열에서의 인원 제거/josephus-permutations.py
+++ 260131/원형 순열에서의 인원 제거/josephus-permutations.py
@@ -3,8 +3,10 @@ n, k = map(int, input().split())
 # Please write your code here.
 from collections import deque
 
-q = deque(range(1, n+1))
+q = deque(range(n, 0, -1))
+answer = []
 while len(q) >= k:
     for _ in range(k-1):
-        deque.appendleft(deque.pop())
-    print(deque.pop())
+        q.appendleft(q.pop())
+    answer.append(q.pop())
+print(' '.join(answer))
