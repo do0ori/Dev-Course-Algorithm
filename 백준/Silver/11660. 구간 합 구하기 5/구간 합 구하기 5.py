60ad4f1fb5b64835dc16c94efb74168978a1497c
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2026-03-05 21:15:26 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1060 ms, Memory: 106832 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,15 @@
+# 2차원 구간 합
+#   D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + A[i][j]
+#   (x1, y1) ~ (x2, y2) 구간 합 = D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1]
+import sys
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(N)]
+D = [[0] * (N + 1) for _ in range(N + 1)]
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i - 1][j - 1]
+for _ in range(M):
+    x1, y1, x2, y2 = map(int, input().split())
+    print(D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1])
