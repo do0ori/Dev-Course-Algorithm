d35c620b47689fab57b62c67e60f879da834b22c
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2026-03-14 22:36:32 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,23 +1,49 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
 N, M = map(int, input().split())
+maze = [list(map(int, input().strip())) for _ in range(N)]
+delta = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-maze = [list(input()) for i in range(N)]
-visit = [[0]*M for j in range(N)]
-queue = [(0,0)]
 
-while(queue):
-    x, y = queue.pop(0)
-        
-    if x-1 >= 0 and visit[x-1][y] == 0 and maze[x-1][y] == '1':
-        queue.append((x-1,y))
-        visit[x-1][y] = visit[x][y] + 1
-    if x+1 < N and visit[x+1][y] == 0 and maze[x+1][y] == '1':
-        queue.append((x+1,y))
-        visit[x+1][y] = visit[x][y] + 1
-    if y-1 >= 0 and visit[x][y-1] == 0 and maze[x][y-1] == '1':
-        queue.append((x,y-1))
-        visit[x][y-1] = visit[x][y] + 1
-    if y+1 < M and visit[x][y+1] == 0 and maze[x][y+1] == '1':
-        queue.append((x,y+1))
-        visit[x][y+1] = visit[x][y] + 1
+# def bfs_intuitive():
+#     visited = [[False] * M for _ in range(N)]
+#     visited[0][0] = True
+#     q = deque([(0, 0)])
+#     step = 0
 
-print(visit[N-1][M-1] + 1)
+#     while q:
+#         for _ in range(len(q)):
+#             r, c = q.popleft()
+
+#             if (r, c) == (N - 1, M - 1):
+#                 return step + 1
+
+#             for dr, dc in delta:
+#                 nr, nc = r + dr, c + dc
+#                 if 0 <= nr < N and 0 <= nc < M and maze[nr][nc] and not visited[nr][nc]:
+#                     visited[nr][nc] = True
+#                     q.append((nr, nc))
+
+#         step += 1
+
+
+# print(bfs_intuitive())
+
+
+def bfs_overwrite():
+    q = deque([(0, 0)])
+
+    while q:
+        r, c = q.popleft()
+
+        for dr, dc in delta:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < N and 0 <= nc < M and maze[nr][nc] == 1:
+                maze[nr][nc] = maze[r][c] + 1
+                q.append((nr, nc))
+
+
+bfs_overwrite()
+print(maze[N - 1][M - 1])
