48ac6324ed692479ef82baa4efbaaa492efaecbf
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2026-03-17 21:41:54 +0900

    [Silver IV] Title: 수 찾기, Time: 160 ms, Memory: 52416 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -2,25 +2,9 @@ import sys
 
 input = sys.stdin.readline
 N = int(input())
-A = list(map(int, input().split()))
-A.sort()
+A = set(map(int, input().split()))
 M = int(input())
 targets = list(map(int, input().split()))
 
-
-def binary_search(target):
-    left, right = 0, N - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if A[mid] > target:
-            right = mid - 1
-        elif A[mid] < target:
-            left = mid + 1
-        else:  # mid == target
-            return 1
-
-    return 0
-
-
 for target in targets:
-    print(binary_search(target))
+    print(1 if target in A else 0)
